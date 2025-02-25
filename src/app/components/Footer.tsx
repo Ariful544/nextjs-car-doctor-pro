27f618e074534426c33c91e5723218fb183cd352
MD. Ariful islam <khidirerbari@gmail.com>
2025-02-25 23:05:01 +0300

    landing page design done

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,53 @@
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+import { FaGoogle, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io";
+
+export default function Footer() {
+  return (
+    <div className="min-h-[400px] md:mt-[130px] px-4 md:px-0  mt-[60px] bg-[#151515]">
+      <div className="max-w-screen-xl h-full pt-32 place-items-center mx-auto grid md:grid-cols-4 grid-cols-2">
+        <div className="space-y-3">
+          <Image
+            src="/assets/logo.svg"
+            width={0}
+            height={0}
+            alt="Car Doctor Logo"
+            className="md:h-16 text-white h-12 w-auto"
+          />
+          <p className="text-[#adadad]">Edwin Diaz is a software and web technologies engineer, a life coach trainer who is also a serial .</p>
+          <div className="flex items-center gap-3">
+            <p className="w-10 h-10 cursor-pointer flex justify-center items-center rounded-full bg-gray-600 text-white"><FaGoogle /></p>
+            <p className="w-10 h-10 cursor-pointer flex justify-center items-center rounded-full bg-gray-600 text-white"><FaTwitter /></p>
+            <p className="w-10 h-10 cursor-pointer flex justify-center items-center rounded-full bg-gray-600 text-white"><IoLogoInstagram /></p>
+            <p className="w-10 h-10 cursor-pointer flex justify-center items-center rounded-full bg-gray-600 text-white"><FaLinkedin /></p>
+          </div>
+        </div>
+        <div className="text-white">
+            <h4 className="text-xl font-semibold">About</h4>
+            <ul className="mt-6 text-[#adadad] space-y-2">
+                <li className="hover:underline"><Link href={"#"}>Home</Link></li>
+                <li className="hover:underline"><Link href={"#"}>Service</Link></li>
+                <li className="hover:underline"><Link href={"#"}>Contact</Link></li>
+            </ul>
+        </div>
+        <div className="text-white">
+            <h4 className="text-xl font-semibold">Company</h4>
+            <ul className="mt-6 text-[#adadad] space-y-2">
+                <li className="hover:underline"><Link href={"#"}>Why Car Doctor</Link></li>
+                <li className="hover:underline"><Link href={"#"}>About</Link></li>
+            </ul>
+        </div>
+        <div className="text-white">
+            <h4 className="text-xl font-semibold">Support</h4>
+            <ul className="mt-6 text-[#adadad] space-y-2">
+                <li className="hover:underline"><Link href={"#"}>Support Center</Link></li>
+                <li className="hover:underline"><Link href={"#"}>FeedBack</Link></li>
+                <li className="hover:underline"><Link href={"#"}>Accesbility</Link></li>
+            </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,6 +27,7 @@ export default function RootLayout({
       <body className={`${inter.className} antialiased`}>
         <Navbar />
         <main className="max-w-screen-xl mx-auto px-4 md:px-0">{children}</main>
+        <Footer/>
       </body>
     </html>
   );
